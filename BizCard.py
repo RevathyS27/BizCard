f18e9901cb3bc3133bface10a56b77c003f6d36a
RevathyS27 <84853075+RevathyS27@users.noreply.github.com>
2024-04-26 19:59:57 +0530

    Update BizCard.py

--- BizCard.py
+++ BizCard.py
@@ -89,6 +89,21 @@ if selected == "Upload & Extract":
 
 
         save_card(uploaded_card)
+      
+       def image_preview(image, res):
+        for (bbox, text, prob) in res:
+            # unpack the bounding box
+            (tl, tr, br, bl) = bbox
+            tl = (int(tl[0]), int(tl[1]))
+            tr = (int(tr[0]), int(tr[1]))
+            br = (int(br[0]), int(br[1]))
+            bl = (int(bl[0]), int(bl[1]))
+            cv2.rectangle(image, tl, br, (0, 255, 0), 2)
+            cv2.putText(image, text, (tl[0], tl[1] - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+        plt.rcParams['figure.figsize'] = (15, 15)
+        plt.axis('off')
+        plt.imshow(image)
 
 
         # DISPLAYING THE UPLOADED CARD
